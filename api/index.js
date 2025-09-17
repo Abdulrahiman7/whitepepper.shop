8b97eadd0a394ff8b003149d0349f7a0a0f700b3
Abdulrahiman7 <rahimanabdul981@gmail.com>
2025-09-17 11:21:28 +0530

    changed

--- api/index.js
+++
@@ -1,5 +0,0 @@
-// This file needs to be converted to TypeScript and use proper imports
-// For now, let's create individual route files for Vercel
-module.exports = (req, res) => {
-  res.status(200).json({ message: "API is working" });
-};
